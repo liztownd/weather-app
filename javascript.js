973fcdf80034f9e3f905db631db879db0e4397d9
Liz Town <liztown@gmail.com>
2021-01-16 15:40:02 -0600

    initial files with pseudocode

---
+++ javascript.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+
+ //on load - get local storage data
+ //render history buttons
+ //load info for most recent search (using last index of history array)   
+//search for city - onclick
+//add city to history
+//get history from local storage
+//check to see if it's been searched before
+//add to list if it's new, send list to local storage
+//generate buttons from search history
+//API calls for current conditions and one for future conditions
+//set data for current conditions
+//append page
+//get data for future conditions
+//append page
+
+
+
+
+
+
+
+
+
+}); //doc ready end tag
